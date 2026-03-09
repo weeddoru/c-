37c931dd8701e3bf75f9fb190e259c63bd76d837
Anastasiia <anastasiiastarosta65@gmail.com>
2026-03-09 08:05:20 +0200

    c

--- LR3_2.cs
+++ LR3_2.cs
@@ -2,13 +2,13 @@ using System;
 namespace LR1{
 class Program3_2
 {
-    static double Cos(double x, double eps = 1e-10)
+    static double Cos(double x, double epsilon = 1e-10)
     {
         x = x * Math.PI / 180;
         double current = 1;
         double sum = 1;
         int n = 1;
-        while (Math.Abs(current) > eps)
+        while (Math.Abs(current) > epsilon)
         {
             current *= -x * x / ((2 * n - 1) * (2 * n));
             sum += current;
